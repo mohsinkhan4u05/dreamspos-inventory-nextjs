14bdb6e3b710f0f91ce657f9bc4cc689e021520a
Mohsn Khan <mohsin4u05@gmail.com>
2026-01-23 18:55:31 +0530

    update copyright year from 2014-2025 to 2025 in sales order list footer

--- src/components/sales/sales-order/SalesOrderList.tsx
+++ src/components/sales/sales-order/SalesOrderList.tsx
@@ -277,7 +277,7 @@ export default function SalesOrderList() {
           </div>
         </div>
         <div className="footer d-sm-flex align-items-center justify-content-between border-top bg-white p-3">
-          <p className="mb-0">2014-2025 © DreamsPOS. All Right Reserved</p>
+          <p className="mb-0">2025 © DreamsPOS. All Right Reserved</p>
           <p>
             Designed &amp; Developed By{" "}
             <Link href="#" className="text-primary">
